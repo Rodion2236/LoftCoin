71594c03fc420dd43127b8da84aa258da0a4c15f
Rodion <kosolapkinr@gmail.com>
2022-06-05 16:33:41 +0400

    completed homework1 and homework2

---
+++ app/src/main/java/com/rodion2236/loftcoin/LoftApp.kt
@@ -0,0 +1,31 @@
+package com.rodion2236.loftcoin
+
+import android.app.Application
+import android.os.StrictMode
+import androidx.viewbinding.BuildConfig
+
+class LoftApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/rodion2236/loftcoin/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.rodion2236.loftcoin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceManager
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+class SplashActivity : AppCompatActivity() {
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val preferenceManager = PreferenceManager.getDefaultSharedPreferences(this)
+
+        compositeDisposable.add(
+            Completable.timer(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    if (preferenceManager.getBoolean(WelcomeActivity.KEY_SHOW_WELCOME_SCREEN, true)) {
+                        startActivity(Intent(this, WelcomeActivity::class.java))
+                    } else {
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }
+                }
+        )
+    }
+
+    override fun onStop() {
+        compositeDisposable.dispose()
+        super.onStop()
+    }
+}
